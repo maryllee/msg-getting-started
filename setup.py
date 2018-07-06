d75c1910695bfb8108b9bc63e7e6d1052d50f2ac
maryllee <38670961+maryllee@users.noreply.github.com>
2018-07-05 22:07:36 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,45 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+    args = {}
+except ImportError:
+    from distutils.core import setup
+    print("""\
+*** WARNING: setuptools is not found.  Using distutils...
+""")
 
+from setuptools import setup
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
+from os import path
+setup(name='my_pkg',
+      version='0.0.1',
+      description='First python package: my_pkg',
+      long_description= "" if not path.isfile("README.md") else read_md('README.md'),
+      author='Mary Lee',
+      author_email='yrameel98@gmail.com',
+      url='https://github.com/maryllee/msg-getting-started',
+      license='MIT',
+      setup_requires=['pytest-runner',],
+      tests_require=['pytest', 'python-coveralls'],
+      install_requires=[
+          "numpy",
+      ],
+      packages=['my_pkg'],
+      # scripts=[''],
+      include_package_data=True,
+      classifiers=[
+          'Development Status :: 1 - planning',
+          'Intended Audience :: Science/Research',
+          'Natural Language :: English',
+          'Operating System :: MacOS',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+      ],
+     )
